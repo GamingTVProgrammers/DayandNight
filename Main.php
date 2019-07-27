cead87f3fa6d09332fb0160d938262c44607035f
GamingTVProgrammers <53368232+GamingTVProgrammers@users.noreply.github.com>
2019-07-27 12:04:14 +0200

    Update Main.php

--- Main.php
+++ Main.php
@@ -3,8 +3,8 @@
 namespace DayandNight\
 
 use pocketmine\Server;
-use pocketmine\plugin\PluginBase
-use pocketmine\command\Sender
-use pocketmine\Command
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Sender;
+use pocketmine\Command;
 
 class
